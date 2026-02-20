4d2fc695bd8e8670df70e7d1697f93a725616df3
Akash-Veerla <akashveerla0@gmail.com>
2026-02-20 22:23:47 +0530

    Fix Map component height=0 issue caused by flex minHeight propagation

--- client/src/components/MapStandalone.jsx
+++ client/src/components/MapStandalone.jsx
@@ -3,11 +3,9 @@ import MapComponent from './Map';
 
 const MapStandalone = () => {
     return (
-        <div className="flex flex-col h-full w-[98%] max-w-[1400px] mx-auto relative transition-colors duration-300">
-            <div className="flex-1 relative w-full h-[calc(100dvh-5rem)] pb-4">
-                <div className="w-full h-full relative overflow-hidden shadow-inner">
-                    <MapComponent />
-                </div>
+        <div className="flex flex-col w-[98%] max-w-[1400px] mx-auto relative transition-colors duration-300 h-[calc(100vh-88px)] pb-6">
+            <div className="w-full h-full relative overflow-hidden shadow-inner">
+                <MapComponent />
             </div>
         </div>
     );
